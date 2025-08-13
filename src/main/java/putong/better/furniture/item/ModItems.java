65a16e967adbcee37b7dd876f0b79998b45e5343
Shadow_PuTong <3321330867@qq.com>
2025-08-13 19:46:36 +0800

    更新

---
+++ src/main/java/putong/better/furniture/item/ModItems.java
@@ -0,0 +1,25 @@
+package putong.better.furniture.item;
+
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+
+import java.util.function.Function;
+
+public class ModItems {
+
+    public static Item register(RegistryKey<Item> key, Function<Item.Settings, Item> factory) {
+        return register(key, factory, new Item.Settings());
+    }
+
+    public static Item register(RegistryKey<Item> key, Function<Item.Settings, Item> factory, Item.Settings settings) {
+        Item item = (Item)factory.apply(settings.registryKey(key));
+        if (item instanceof BlockItem blockItem) {
+            blockItem.appendBlocks(Item.BLOCK_ITEMS, item);
+        }
+
+        return Registry.register(Registries.ITEM, key, item);
+    }
+}
